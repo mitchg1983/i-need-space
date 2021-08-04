a88d14cab0a345a94ad67b37608eb086e459030f
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-08-04 12:41:17 -0400

    Fix time display problem

---
+++ main.js
@@ -0,0 +1,72 @@
+//Declare dom elements via jquery
+const search = $("#search");
+
+function displayTime(i) {
+
+    // console.log(i);
+    // console.log(typeof i);
+
+    return i.toString().substring(0, 19);
+};
+
+//Will use the three text fields as inputs, to return the desired sattelite info
+//when the 'search' button is clicked
+$(search).on("click", function () {
+  //These are declared inside the scope of the function, so the user can re-enter
+  //coords without reloading the page.
+  const apiKey = $("#api-key").val();
+  const address = $("#address").val();
+  const addressURI = encodeURI(address);
+  const norad = $("#norad").val();
+
+  //Using the async function
+  const search = async () => {
+    //Use the address input, after encoding for URI, to fetch the lat & log coordinates from mapbox
+    const rawData = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${addressURI}.json?access_token=${apiKey}`
+    );
+
+    const coords = await rawData.json();
+
+    //Inside the return from mapbox, are the desired coords
+    const longitude = coords.features[0].center[0];
+    const latitude = coords.features[0].center[1];
+
+    console.log(
+      "Address converted to coordinates: ",
+      latitude,
+      "by",
+      longitude
+    );
+
+    //Use the coords from mapbox, and the NORAD sat identifier, to fetch our satellite data
+    const rawSatData = await fetch(
+      `https://satellites.fly.dev/passes/${norad}?lat=-${latitude}&lon=${longitude}&limit=1&days=15&visible_only=true`
+    );
+
+    const flyover = await rawSatData.json();
+
+    console.log(flyover);
+
+    //Store the sat flyover data
+    const rise = flyover[0].rise.utc_datetime;
+    const culminate = flyover[0].culmination.utc_datetime;
+    const set = flyover[0].set.utc_datetime;
+
+    // console.log(
+    //   "Satellite", norad, "will rise at -", rise,
+    //   "\n",
+    //   "Satellite", norad,
+    //   "will culminate at -", culminate,
+    //   "\n",
+    //   "Satellite",  norad,  "will set at -", set,
+    //   "\n"
+    // );
+
+    console.log("Your rise time is,", displayTime(rise));
+    console.log("Your culminate time is,", displayTime(culminate));
+    console.log("Your set time is,", displayTime(set));
+  };
+
+  search();
+});
